838dfc8356c666e737443bc43846d0340be5a1f8
samclee <samclee@gmail.com>
2020-06-22 18:51:08 -0700

    Start stop button logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 enum TimerState {
@@ -7,26 +7,32 @@ enum TimerState {
 }
 
 function App() {
+  const [timerState, setTimerState] = useState(TimerState.Stopped)
+
+  const onStartStopButtonPress = () => {
+    if (timerState === TimerState.Running) {
+      setTimerState(TimerState.Stopped)
+      console.log("stopped")
+    } else if (timerState === TimerState.Stopped) {
+      setTimerState(TimerState.Running)
+      console.log("running")
+    }
+  }
+
+  const startStopText = (timerState === TimerState.Running) ? "Stop" : "Start";
+
   return (
     <div className="TimerApp">
       <h1>Knob</h1>
       <input type="text" />
       <div>
-        <button>start/stop</button>
+        <button onClick={onStartStopButtonPress}>{startStopText}</button>
         <button>reset</button>
       </div> 
       <input type="checkbox" />
       <label>Reset on timeout</label>
     </div>
   );
-}
-
-function stopTimer() {
-
-}
-
-function startTimer() {
-
 }
 
 export default App;
